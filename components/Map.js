d5e5a8ae27209b9cd61251366d519d12502cdb12
Deepak8858 <88921480+Deepak8858@users.noreply.github.com>
2022-03-16 19:33:32 +0530

    lost commit

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useEffect } from "react"
+import mapboxgl from "mapbox-gl"
 
-const map = () => {
-    return (
-        <div>map</div>
-    )
+const style = {
+    wrapper: 'flex-1 h-full w-full',
 }
 
-export default map
+mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN
+
+
+
+
+const Map = () => {
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/deepak8858/cl0ib79kc003915pji9zlrm75',
+            center: [-73.935242, 40.730610],
+            zoom: 3,
+        })
+
+
+    }, [])
+
+    return <div className={style.wrapper} id='map' />
+
+}
+
+export default Map
