ad5bb55b606ec5face8d90bc5934f94880693ab8
Deepak8858 <88921480+Deepak8858@users.noreply.github.com>
2022-03-16 20:47:28 +0530

    low commit

--- components/Map.js
+++ components/Map.js
@@ -1,11 +1,12 @@
-import { useEffect } from "react"
-import mapboxgl from "mapbox-gl"
+import { useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
 
 const style = {
     wrapper: 'flex-1 h-full w-full',
 }
 
-mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
 
 
 
