aebd5bd99585e02f4e9f372e8c062b3870081e13
Deepak8858 <88921480+Deepak8858@users.noreply.github.com>
2022-03-19 20:40:41 +0530

    no commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,10 +5,11 @@ import { BsPerson } from 'react-icons/bs'
 const style = {
     wrapper: 'h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20',
     leftMenu: 'flex gap-3',
-    logo: 'text-3xl text-white flex cursor-pointer mr-16',
-    menuItem: 'text-lg text-white font-medium flex items-center mx-4 cursor-pointer',
+    logo: 'text-3x1 text-white flex cursor-pointer mr-16',
+    menuItem: 'text-1g text-white font-medium flex items-center mx-4 cursor-pointer',
     rightMenu: 'flex gap-3 items-center',
     userImageContainer: 'mr-2',
+    userImage: 'h-10 w-10 rounded-full p-px object-cover cursor-pointer',
     loginButton: 'flex item-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1',
     loginText: 'm1-2',
 
