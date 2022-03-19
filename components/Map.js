83b614220b0b7b96ffb73ff4ba113ff73ad6ea42
Deepak8858 <88921480+Deepak8858@users.noreply.github.com>
2022-03-19 19:43:41 +0530

    shit commit

--- components/Map.js
+++ components/Map.js
@@ -5,7 +5,7 @@ const style = {
     wrapper: 'flex-1 h-full w-full',
 }
 
-mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
 
 const Map = () => {
