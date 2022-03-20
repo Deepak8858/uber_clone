8fe53395387cd48e228d8bd239e5bb12d1ab1782
Deepak8858 <88921480+Deepak8858@users.noreply.github.com>
2022-03-20 19:38:42 +0530

    or commit

--- components/LocationSelector.js
+++ components/LocationSelector.js
@@ -3,6 +3,15 @@ import { useState } from 'react'
 
 const style = {
     wrapper: 'pt-2',
+    searchHeader: 'w-full font-bold text-left flex items-center text-3xl p-4 overflow-hidden',
+    inputBoxes: 'flex flex-col mb-4 relative',
+    inputBox: 'h-10 mx-4 border-2 bg-[#eeeeee] flex item-center my-1 py-1 px-2',
+    focusedInputBox: 'border-black',
+    focusedInputBox: 'border-black',
+    input: 'my-2 rounded-2 p-2 outline-back border absolute z-10 left-[2.3rem] top-[2rem]',
+    verticalLine: 'w-0 h-[2rem] border-black border  absolute z-10 left-[2.3rem] top-[2rem]',
+    horizontalLine: 'w-[2rem] h-0 border-black border  absolute z-10 left-[2.3rem] top-[2rem]',
+
 
 }
 
