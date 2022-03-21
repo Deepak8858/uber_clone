429ef1c6739ba712b8882cf316bd7ad0d30407b5
Deepak8858 <88921480+Deepak8858@users.noreply.github.com>
2022-03-21 18:53:17 +0530

    he commit

--- components/Map.js
+++ components/Map.js
@@ -1,27 +1,44 @@
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 import mapboxgl from 'mapbox-gl'
+import { UberContext } from '../context/uberContext'
 
 const style = {
-    wrapper: 'flex-1 h-full w-full',
+  wrapper: `flex-1 h-full w-full`,
 }
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
-
 const Map = () => {
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/deepak8858/cl0ib79kc003915pji9zlrm75',
-            center: [-73.935242, 40.730610],
-            zoom: 3,
-        })
+  const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
 
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    })
 
-    }, [])
+    if (pickupCoordinates) {
+      addToMap(map, pickupCoordinates)
+    }
 
-    return <div className={style.wrapper} id='map' />
+    if (dropoffCoordinates) {
+      addToMap(map, dropoffCoordinates)
+    }
 
+    if (pickupCoordinates && dropoffCoordinates) {
+      map.fitBounds([dropoffCoordinates, pickupCoordinates], {
+        padding: 400,
+      })
+    }
+  }, [pickupCoordinates, dropoffCoordinates])
+
+  const addToMap = (map, coordinates) => {
+    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
+  }
+
+  return <div className={style.wrapper} id='map' />
 }
 
 export default Map
