2771d4c4fd9c2fd241effd421d21a20163ec9c4b
Deepak8858 <88921480+Deepak8858@users.noreply.github.com>
2022-03-21 19:09:10 +0530

    hh commit

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,11 +1,16 @@
+// First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
+
+// Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-import { userSchema } from './userSchema'
-import { ridesSchema } from './ridesSchema'
-import { tripSchema } from './tripSchema'
-
+// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
+  // We name our schema
   name: 'default',
-  types: schemaTypes.concat([userSchema, ridesSchema, tripSchema]),
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    /* Your types here! */
+  ]),
 })
